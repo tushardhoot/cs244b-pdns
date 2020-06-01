f803daa40da6e044d4f951214c3c8991c97fed0a
Tushar Dhoot <tdhoot@stanford.edu>
2020-05-31 23:58:47 -0700

    fix commandline parsing

--- grpc_server/src/main/java/edu/cs244b/server/Main.java
+++ grpc_server/src/main/java/edu/cs244b/server/Main.java
@@ -1,12 +1,9 @@
 package edu.cs244b.server;
 
 import edu.cs244b.mappings.DNSMappingStore;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
+
+import java.util.Arrays;
 
 
 public class Main {
@@ -17,18 +14,21 @@ public class Main {
         portOption.setRequired(false);
         options.addOption(portOption);
 
-        Option dnsOption = new Option("d", "dnsBacked", false, "Whether this server is backed by normal DNS resolution");
+        Option dnsOption = new Option("d", false, "Whether this server is backed by normal DNS resolution");
         dnsOption.setRequired(false);
         options.addOption(dnsOption);
 
         CommandLine commandLine;
-        CommandLineParser parser = new BasicParser();
+        CommandLineParser parser = new DefaultParser();
 
         try
         {
             commandLine = parser.parse(options, args);
             int port = Integer.parseInt(commandLine.getOptionValue("port", "8980"));
-            boolean dnsBacked = Boolean.parseBoolean(commandLine.getOptionValue("dnsBacked", "false"));
+            boolean dnsBacked = commandLine.hasOption("d");
+
+            System.out.println("args " + Arrays.toString(args));
+            System.out.println("dnsBacked " + dnsBacked);
 
             DomainLookupServer server;
             if (!dnsBacked) {
